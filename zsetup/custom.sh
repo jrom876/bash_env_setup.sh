40e79363e841575189311901ec8c722a20667d1c
jrom876 <jrom876@gmail.com>
2021-03-02 12:20:21 -0700

    Update custom.sh

--- zsetup/custom.sh
+++ zsetup/custom.sh
@@ -8,23 +8,29 @@
 # Github Page:		www.github.com/jrom876/setup_env/zsetup/custom.sh
 #
 ############################################
-### PURPOSE: CODE DEVELOPMENT EFFICIENCY ### 
+### PURPOSE: CODE DEVELOPMENT EFFICIENCY ###
 ############################################
 
 # These are aliases customized for the developer's convenience.
 # They should be modified before use of this script in new systems.
 
-# There are 3 COMMAND classes:
+# There are 2 COMMAND classes:
 
 # 1) MOVE TO LOCATION COMMANDS
 #	-- these scripts allow fast navigation between directory locations
 
-# 2) COMPOUND COMMANDS 
-#	-- these scripts provide easy compilation and execution of 
+# 2) COMPOUND COMMANDS
+#	-- these scripts provide easy compilation and execution of
 # 	   code for high-speed development
 
-# 3) GIT COMMANDS (IN DEVELOPMENT)
-#	-- these scripts will automate some of the git functions 
+##################################
+shopt -s expand_aliases
+##################################
+
+########################
+### GLOBAL VARIABLES ###
+########################
+## None
 
 #######################################################
 ### MOVE TO LOCATION COMMANDS ###
@@ -32,18 +38,11 @@
 #######################################################
 # main locations #
 alias mth='cd ~/'
-alias mtftp='cd /home/ftpuser/'
-alias mtsftp='cd /home/sftpuser/'
 alias mroot='cd /'
 alias mtetc='cd /etc/'
 
-# media locations #
-alias mtm='cd /media/$USER/'
-# alias mtkj='cd /media/jrom/YG64_CACHE/kjversion'
-
-# other locations #
-alias mta='cd ~/Arduino/'
-alias mts='cd ~/sandbox/'
+alias mtm='cd /media/$USER/; ls -la'
+alias mts='cd ~/sandbox/; ls -la'
 
 #################################
 ####### COMPOUND COMMANDS #######
@@ -52,7 +51,10 @@ alias mts='cd ~/sandbox/'
 alias ccgui='cd ~/laserDriver; \
 		make -f make-gui.mk guion \
 		cd $OLDPWD'
-
+		
+alias ccfui='cd ~/laserDriver; \
+		python3 labjackgui.py'
+		
 alias cclaser='cd ~/laserDriver; \
 		gcc -g -o laserPointer laserPointer.c -lm `pkg-config --cflags --libs gtk+-2.0`; \
 		./laserPointer; \
@@ -62,20 +64,83 @@ alias ccprimeNums='cd ~/_files/primeNum; \
 		g++ -g -o primeNum primeNum.cpp -lm; \
 		./primeNum; echo;\
 		cd $OLDPWD'
-
+		
 alias ccsciCalc='cd ~/sandbox/sciCalc-master/sciCalc; \
 		make -f mc.mk; \
  		./main; \
 		cd $OLDPWD'
 
 ## Python Programs
-alias ccdtb='cd ~/_files/decToBinCalculator_master; \
+alias ccdtb='cd ~/sandbox/decToBinCalculator_master; \
 		python3 decToBinCalc.py; \
 		cd $OLDPWD'
+alias cctp='cd ~/sandbox/Python_Person_master/TestPerson; \
+		python3 TestPerson.py; \
+		cd $OLDPWD'
 
-alias cctp='cd ~/_files/TestPerson-master/TestPerson; \
-		python3 TestPerson.py'
+## Coding Challenge 
 
+alias cccc='cd ~/temp/coding_challenge; \
+		python3 vlanout.py'
+
+alias ccyycc='cd ~/temp/coding_challenge; \
+		python3 -m pdb vlanout.py'
+		
+alias ccddcc='cd ~/temp/coding_challenge; \
+		python3 driver1.py'
+		
+alias ccbapp='cd /var/www/CESApp; \
+		sudo bash start.sh'	
+				
+alias ccfapp='cd /var/www/CESApp; \
+		sudo docker cesapp.test'	
+			
+alias ccvapp='cd /var/www/TestApp; \
+		sudo bash start.sh'
+		
+alias ccruise='cd ~/cruiseAuto; \
+		python3 oldmycode.py'
+			
+alias ccintcalc='cd ~/intCalc; \
+		python3 csvgen.py'
+			
+alias cczintcalc='cd ~/intCalc; \
+		python3 top_gui.py'
+		
+alias ccmparse='cd ~/psetup; \
+		python3 parse_input.py
+		cd $OLDPWD'	
+
+alias ccquil='cd ~/statics; \
+		python3 equil.py
+		cd $OLDPWD'
+
+alias ccunits='cd ~/statics; \
+		python3 units.py
+		cd $OLDPWD'
+			
+####################################
+
+ecxstom () {
+	echo
+	echo 'CUSTOM COMPOUND COMMANDS'; echo
+	echo 'COMMANDS	EXPLANATION'
+	echo 'ccbapp		sudo bash start.sh'
+	echo 'cccc		python3 vlanout.py'
+	echo 'ccyycc		python3 -m pdb vlanout.py'
+	echo 'cclaser		./laserPointer'
+	echo 'ccgui		make -f make-gui.mk guion'
+	echo 'ccdtb		python3 decToBinCalc.py'
+	echo 'ccprimeNums	~/_files/primeNum ./primeNum'
+	echo 'ccsciCalc	~/sandbox/sciCalc-master/sciCalc make -f mc.mk; ./main'
+	echo 'cctp		~/sandbox/Python_Person_master/TestPerson python3 TestPerson.py'
+	echo 'ccruise		~/cruiseAuto python3 oldmycode.py'
+	echo 'ccintcalc	~/intCalc python3 csvgen.py'
+	echo 'cczintcalc	~/intCalc python3 top_gui.py'
+	echo 'ccquil		~/statics python3 equil.py'
+	echo 'ccunits		~/statics python3 units.py'; echo
+}
+	
 ####################################################
 
 ############################
@@ -115,8 +180,8 @@ gitchkout () {
 # [hotfix 1fb7853] Fix broken email address
 # 1 file changed, 2 insertions(+)
 
-# You can run your tests, make sure the hotfix is what you want, 
-# and finally merge the hotfix branch back into your master branch 
+# You can run your tests, make sure the hotfix is what you want,
+# and finally merge the hotfix branch back into your master branch
 # to deploy to production. You do this with the git merge command:
 
 # git checkout master
@@ -126,15 +191,15 @@ gitchkout () {
 # index.html | 2 ++
 # 1 file changed, 2 insertions(+)
 
-# You’ll notice the phrase “fast-forward” in that merge. 
-# Because the commit C4 pointed to by the branch hotfix you merged in 
-# was directly ahead of the commit C2 you’re on, Git simply moves the 
-# pointer forward. To phrase that another way, when you try to merge 
-# one commit with a commit that can be reached by following the first 
-# commit’s history, Git simplifies things by moving the pointer forward 
+# You’ll notice the phrase “fast-forward” in that merge.
+# Because the commit C4 pointed to by the branch hotfix you merged in
+# was directly ahead of the commit C2 you’re on, Git simply moves the
+# pointer forward. To phrase that another way, when you try to merge
+# one commit with a commit that can be reached by following the first
+# commit’s history, Git simplifies things by moving the pointer forward
 # because there is no divergent work to merge together — this is called a “fast-forward.”
 
-# Your change is now in the snapshot of the commit pointed to 
+# Your change is now in the snapshot of the commit pointed to
 # by the master branch, and you can deploy the fix.
 
 ###################################
