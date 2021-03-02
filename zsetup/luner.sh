d4b45968def3e7450989f5c29a1781a946e3c06f
jrom876 <jrom876@gmail.com>
2021-03-02 12:26:00 -0700

    Update luner.sh

--- zsetup/luner.sh
+++ zsetup/luner.sh
@@ -11,9 +11,15 @@
 #############################################
 ####### PURPOSE: SYSTEM INTERROGATION #######
 #############################################
+
 ## This automated test routine displays a broad range
 ## of pertinent system information in a coherent format
 ## for the convenience of the System Administrator.
+## It must be modified for each system.
+
+##################################
+shopt -s expand_aliases
+##################################
 
 ### local user network evaluation routine (luner)
 luner () {
@@ -27,7 +33,8 @@ luner () {
 	echo '======= UFW ======='; echo;
 	sudo ufw status;
 	echo 'Available Entropy:'; entropy; echo;
-	echo 'Crash Dump Report:'; crsh; savcrsh; echo;
+	echo 'Crash Dump Report:'; crsh; 
+	savcrsh; echo;
 
 	echo '======= Environment ======='; echo;
 #	printenv;
@@ -40,48 +47,62 @@ luner () {
 	linv; echo;
 	echo 'Jeston Nano NVIDIA Version'; jtv; echo;
 	echo 'Ubuntu Version'; ubv; echo;
-	gcc --version| grep "gcc " -A 1; echo;
-#	g++ --version;
-	gdb -v | grep "GNU gdb" -A 1; echo;
-	python --version; python3 --version; echo;
-	echo 'JDK Version: '; java --version; echo;
-	perl --version | grep  "This\ is\ perl" -A 1; echo;
+	gcc --version| grep -F "gcc " -A 1; echo;
+	g++ --version;
+	gdb -v | grep -F "GNU gdb" -A 1; echo;
+	python --version; python3 --version;
+	jpyv=$( jupyter --version ); echo "jupyter $jpyv"; echo;
+	njsv=$( node --version ); echo "node.js $njsv"; echo;
+	java --version; echo;
+	perl --version | grep  "This\ is\ perl"; echo;
 	ssh -V; echo;
 	tar --version | grep -E tar; echo;
-	git --version;
-	# nginx -v;
-	nmap -V; echo;
-	echo 'Traceroute Version:'; traceroute -V; echo;
+	git --version; echo;
+	docker --version; echo;
+	shwflask; echo;
+#	flask --version; echo;
+#	nginx -v;
+	nmap -V | grep -F "Nmap " -A 1; echo;
+	traceroute -V; echo;
 
+	echo '======= Fan and Temperature Status ======='; echo;
+	#gtfanspeed; echo
+	#sensors
+	fanstatus; echo
+	
 	echo '======= CPU ======='; echo;
 	lscpu | grep Arch -A 23; echo;
 #	getsn;
+	inxi -F; echo;
 
 	echo '======= Hard Drive ======='; echo;
 	df -H | grep Filesystem -A 6; echo ;
 
 	echo '======= RAM ======='; echo;
-	free -t; echo;
-	pmem | grep MemTotal -A 5; echo;
+	free -t -h B; echo;
+#	pmem | grep MemTotal -A 5; echo;
 
 	echo '======= PCI Devices ======='; echo;
 	lspci -tv; echo; # Show tree structure verbose
 	# lspci -k; echo; # Show kernel drivers
 
+	echo '======= Systemctl Failures ======='; echo;
+	gtfunit; echo
+	
 	echo '======= Ports and Processes ======='; echo;
-	echo 'Listing active USB ports:';	getUSB;	echo;
-	# echo 'Listing active Serial ports:';	getSER;	echo;
+#	echo 'Listing active USB ports:';	getUSB;	echo;
+	echo 'Listing active Serial ports:';	getSER;	echo;
 #	getTTy; echo;
 #	ss -ltn; echo;
-	sudo netstat -tulpn; echo;
-	# sudo dmidecode -t 8; echo;
-	# lsoi; echo;
+	sudo netstat -tulp; echo;
+#	sudo dmidecode -t 8; echo;
+#	lsoi; echo;
 
 	echo '======= Network Routing Information ======='; echo;
 	getIP; echo;
 	echo 'ARP Table:'; arp; echo;
 	savzulu; echo; # HP-250 and Jetson Nano
-	# savwpa; echo; # RaspberryPi
+#	savsupp; echo; # RaspberryPi
 	# echo 'System ARP cache:'; pnarp; echo;
 	# echo 'System ARP table:'; ipn;	echo;
 	echo 'System Routing table:'; ip route show; echo;
