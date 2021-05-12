73a4102adadd331316da7a590fd54626d4ef457c
jrom876 <jrom876@gmail.com>
2021-05-12 06:34:05 -0600

    Update runinstall.sh

--- runinstall.sh
+++ runinstall.sh
@@ -13,6 +13,37 @@
 # An improved method to setup the Linux/Unix terminal environment 
 # with custom aliases and bash scripts for quicker function calls,
 # efficient file navigation, and better system control.
+
+###############################################################
+###############################################################
+###############################################################
+#
+# Copyright (C) 2019, 2021 
+# Jacob Romero, Creative Engineering Solutions, LLC
+# cesllc876@gmail.com
+# admin@jrom.io 
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public 
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# 
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to:
+# The Free Software Foundation, Inc.
+# 59 Temple Place, Suite 330
+# Boston, MA 02111-1307 USA
+#
+#
+###############################################################
+###############################################################
+###############################################################
+
 ####################
 ### INSTRUCTIONS ###
 # In order to run this script in a given CLI: 
