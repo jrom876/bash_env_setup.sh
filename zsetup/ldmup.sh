eaa8d748b65629fca31b03ee2e4935eb2a6e3b3b
jrom876 <jrom876@gmail.com>
2021-05-12 06:31:36 -0600

    Update ldmup.sh

--- zsetup/ldmup.sh
+++ zsetup/ldmup.sh
@@ -29,6 +29,37 @@
 # NOTE: Don't forget the important dot-space at the beginning of the
 # command because this designates the current CLI as the target.
 #
+
+###############################################################
+###############################################################
+###############################################################
+#
+# Copyright (C) 2019, 2021 
+# Jacob Romero, Creative Engineering Solutions, LLC
+# cesllc876@gmail.com
+# admin@jrom.io 
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public 
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# 
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to:
+# The Free Software Foundation, Inc.
+# 59 Temple Place, Suite 330
+# Boston, MA 02111-1307 USA
+#
+#
+###############################################################
+###############################################################
+###############################################################
+
 ##################################
 shopt -s expand_aliases
 ##################################
