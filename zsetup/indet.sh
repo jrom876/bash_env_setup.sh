e12e7c4a1a35522ea1ed7933f00228572f2cfd56
jrom876 <jrom876@gmail.com>
2021-07-23 23:38:24 -0600

    Update indet.sh

--- zsetup/indet.sh
+++ zsetup/indet.sh
@@ -19,6 +19,36 @@
 ## 2. 	place some of the extracted data in reports 
 ## 3. 	provide cat calls for easy retreival of the reports
 
+###############################################################
+###############################################################
+###############################################################
+#
+# Copyright (C) 2019, 2021 
+# Jacob Romero, Creative Engineering Solutions, LLC
+# cesllc876@gmail.com
+# admin@jrom.io 
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public 
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# 
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with this program; if not, write to:
+# The Free Software Foundation, Inc.
+# 59 Temple Place, Suite 330
+# Boston, MA 02111-1307 USA
+#
+#
+###############################################################
+###############################################################
+###############################################################
+
 ####################
 #### REFERENCES ####
 ####################
@@ -50,36 +80,6 @@
 
 ## IMPORTANT FOR PYTHON-TO-BASH SCRIPTING!!
 ## https://stackoverflow.com/questions/14155669/call-python-script-from-bash-with-argument
-
-###############################################################
-###############################################################
-###############################################################
-#
-# Copyright (C) 2019, 2021 
-# Jacob Romero, Creative Engineering Solutions, LLC
-# cesllc876@gmail.com
-# admin@jrom.io 
-#
-# This program is free software; you can redistribute it
-# and/or modify it under the terms of the GNU General Public 
-# License as published by the Free Software Foundation, version 2.
-#
-# This program is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied 
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# 
-# See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public
-# License along with this program; if not, write to:
-# The Free Software Foundation, Inc.
-# 59 Temple Place, Suite 330
-# Boston, MA 02111-1307 USA
-#
-#
-###############################################################
-###############################################################
-###############################################################
 
 ##################################
 shopt -s expand_aliases
