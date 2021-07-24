6b9dec65e2dbeb06bc7ab4dc9a9377aeb4b86cb5
jrom876 <jrom876@gmail.com>
2021-07-23 23:35:34 -0600

    Update cwf.sh

--- zsetup/cwf.sh
+++ zsetup/cwf.sh
@@ -58,110 +58,6 @@
 shopt -s expand_aliases
 ##################################
 
-## First, use 'evproc' or 'netstat -tulp' to see all ports,
-## processes and services.
-## Then pick the process or service you want to start, stop or investigate.
-evproc () {
-	echo;
-	sudo netstat -tulp; echo
-	lsoi; echo;
-	sudo ufw status;
-	echo 'Available Entropy:'; entropy; echo;
-	echo 'Crash Dump Report:'; crsh; savcrsh; echo;
-	## echo 'Open Internet Ports:'; lsoi; echo;
-	# lsTTy; echo;
-	# getUSB;
-	# getSER; echo;
-	# getnet; echo;
-	# cwShowpw; echo;
-	# savzulu; echo; # HP-250 and Jetson Nano
-	# savxray; echo;
-	savsupp; echo; # RaspberryPi
-	echo 'Available Commands:'
-	echo 'cwdsabl $1     or   svkill $1          to disable or kill a process'
-	echo 'cwenabl $1     or   svstart $1         to enable or start a process'
-	echo 'ufenable                               to enable firewall'
-	echo 'svrestart $1                           to restart a service'
-	echo 'kusb $1        or   kport $1           to kill a USB or other port'
-	echo 'denyPort $1    or   sudo ufw deny $1   to deny a port, process, or address'
-	echo 'allowPort $1   or   sudo ufw allow $1  to allow a port, process, or address'
-	echo 'pk9_signal $1                          to kill a signal'
-	echo 'svtus $1                               to get status of a service'
-	echo 'svall                                  to list all running services'
-	echo 'psrch $1                               to search current running processes for given keyword'
-}
-
-evsec () {
-	echo; currDate; echo;
-	echo '======= Users ======='; echo;
-	wami; echo;
-	echo "Hostname:"; hn; echo;
-#	echo 'Groups:'; grpid; echo;
-#	echo 'Linux Users:'; lusers; echo;
-	echo '======= UFW ======='; echo;
-	sudo ufw status;
-	echo 'Available Entropy:'; entropy; echo;
-#	echo 'Crash Dump Report:'; crsh; savcrsh; echo;
-
-<<COMMENT
-	echo '======= CPU ======='; echo;
-	inxi -F; echo;
-COMMENT
-
-	echo '======= Ports and Processes ======='; echo;
-#	echo 'Listing active USB ports:';	getUSB;	echo;
-	echo 'Listing active Serial ports:';	getSER;	echo;
-#	getTTy; echo;
-#	ss -ltn; echo;
-	sudo netstat -tulp; echo;
-	# sudo dmidecode -t 8; echo;
-	lsoi; echo;
-
-	echo '======= Network Information ======='; echo;
-	getIP
-#	echo 'ARP Table:'; arp; echo;
-	# savzulu; echo; # HP-250 and Jetson Nano
-	savsupp; echo; # RaspberryPi
-	sudo netstat -i; echo; # Kernel interface table
-#	sudo netstat -r; echo; # Kernel routing table
-#	getnet; echo;
-	echo '================ Useful Commands ================'
-	echo 'COMMON COMMANDS'
-	echo 'eclist		Table of Contents'
-	echo 'luner		comprehensive system evaluation'
-	echo 'lusers		show all linux system users'
-	echo 'ufenable	sudo ufw enable'
-	echo 'lssysbkups	list primary backups'
-	echo 'getnet		simple network evaluation'
-	echo 'evnet		specific network evaluation'
-	echo 'fevnet		full network evaluation'
-	echo 'gtzulu		show network passwd (HP-250 and Jetson Nano)'
-	echo 'gtsupp		show network passwd (Raspberry Pi)'; echo
-
-	echo 'CAT CALLS	FILE DISPLAYED'
-	echo 'ctnmapl		nmap-list'
-	echo 'ctgw		Gateway-list'
-	echo 'cthp250		HP250-list'
-	echo 'ct12		RPi12-list'
-	echo 'ct13		RPi13-list'
-	echo 'ctjetson	Jetson-list'
-	echo 'ctzero		PiZero_W-list'
-	echo 'ctcrsh		crash_dump_report'; echo
-
-	echo 'PROCESS CONTROL COMMANDS'
-	echo 'cwdsabl $1     or   svkill $1          to disable or kill a process'
-	echo 'cwenabl $1     or   svstart $1         to enable or start a process'
-	echo 'svrestart $1                           to restart a service'
-	echo 'kusb $1        or   kport $1           to kill a USB or other port'
-	echo 'denyPort $1    or   sudo ufw deny $1   to deny a port, process, or address'
-	echo 'allowPort $1   or   sudo ufw allow $1  to allow a port, process, or address'
-	echo 'pk9_signal $1                          to kill a signal'
-	echo 'svtus $1                               to get status of a service'
-	echo 'svall                                  to list all running services'
-	echo 'psrch $1                               to search current running processes for given keyword'
-
-}
-
 #### Disabling/Enabling a Service ####
 cwdsabl () {
 	sudo systemctl disable $1
@@ -420,7 +316,6 @@ dltsupp () {
 ##           sudo nano /etc/shadow         ##
 ############\\\\\\\\\\|//////////############
 
-
 ###########################
 #### TABLE OF CONTENTS ####
 ###########################
@@ -603,7 +498,6 @@ cwnfb () {
 ## Example: 'cwTB 1h' ==> would remove the root directory in 1 hour
 ## sleep <arg> can be 1s, 1m, 1h, 1d, etc. (default = ms)
 
-
 ##############################################
 ##-------<<<<< SR TIMED DISABLE >>>>>-------##
 ## A less drastic time bomb attack which stops systemd-resolved,
@@ -611,7 +505,6 @@ cwnfb () {
 ## Format: 'cwSRTD <arg1 = time delay before cwksr>'
 ## Example: 'cwSRTD 1h' ==> would stop systemd-resolved in 1 hour
 ## sleep <arg> can be 1s, 1m, 1h, 1d, etc. (default = ms)
-
 
 ####################################################
 ##-------<<<<< ZEROING ALL PARTITIONS >>>>>-------##
@@ -636,7 +529,6 @@ cwnfb () {
 ##-------<<<<< ROOT PROTECTION REMOVAL >>>>>-------##
 ## A script that removes all file protections and
 # makes all files in a system executable.
-
 
 ########################################################
 ##-------<<<<< ROOT EXECUTABILITY REMOVAL >>>>>-------##
